50c8008291093f1f6e69a984e5eae23a0f6081a0
ibala <balaxyz@ymail.com>
2022-01-03 18:22:23 +0530

    Update HomeController.cs
    not using this file.

--- HotelListing/Controller/HomeController.cs
+++ HotelListing/Controller/HomeController.cs
@@ -1,16 +1,1 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace HotelListing.Controller
-{
-    public class HomeController : Controller
-    {
-        public IActionResult Index()
-        {
-            return View();
-        }
-    }
-}
+don't use this file
